292235110b7379e18180f093d4f265f2c06da700
r611171 <edward.cheadle@cambiahealth.com>
2017-04-07 12:33:42 -0600

    S5L45 Challenge

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 const port = 3000;
 
 var app = express();
@@ -7,6 +9,22 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  })
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -21,14 +39,12 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to the HOS',
-    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) =>{
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
